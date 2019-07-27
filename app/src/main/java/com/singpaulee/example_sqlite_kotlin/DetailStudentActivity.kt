28706ea3d10ac16f97792bc12ef2f8402fd33599
singpaulee <hanifabdullah7@gmail.com>
2019-07-27 14:24:38 +0700

    5. Create activity DetailStudentActivity and setting the layout

---
+++ app/src/main/java/com/singpaulee/example_sqlite_kotlin/DetailStudentActivity.kt
@@ -0,0 +1,14 @@
+package com.singpaulee.example_sqlite_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_detail_student.*
+
+class DetailStudentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_student)
+    }
+}
