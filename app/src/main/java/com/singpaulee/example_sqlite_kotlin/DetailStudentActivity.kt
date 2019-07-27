665cdcea063a438e255161859b5bd3ec4af322b4
singpaulee <hanifabdullah7@gmail.com>
2019-07-27 14:30:28 +0700

    12. Set up class DetailStudentActivity.kt

--- app/src/main/java/com/singpaulee/example_sqlite_kotlin/DetailStudentActivity.kt
+++ app/src/main/java/com/singpaulee/example_sqlite_kotlin/DetailStudentActivity.kt
@@ -10,5 +10,13 @@ class DetailStudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_student)
+        
+        val student = intent.getParcelableExtra<StudentContract>("student")
+        Log.d("STUDENT", student.toString())
+
+        dsa_tv_name.text = student.name
+        dsa_tv_age.text = "${student.age} tahun"
+        dsa_tv_address.text = student.address
+        dsa_tv_major.text = student.majority
     }
 }
