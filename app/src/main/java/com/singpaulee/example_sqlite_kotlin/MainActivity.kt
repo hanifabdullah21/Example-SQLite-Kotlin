ac4fca1c4464b291af32615e5e182d0dea57f9b3
singpaulee <hanifabdullah7@gmail.com>
2019-07-28 10:33:34 +0700

    Update MainActivity

--- app/src/main/java/com/singpaulee/example_sqlite_kotlin/MainActivity.kt
+++ app/src/main/java/com/singpaulee/example_sqlite_kotlin/MainActivity.kt
@@ -12,6 +12,8 @@ import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
+    var adapter: StudentAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,22 +21,33 @@ class MainActivity : AppCompatActivity() {
         val list = getListDataStudent()
 
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        val adapter = StudentAdapter(this, list)
+        adapter = StudentAdapter(this, list)
 
         main_rv_list_data.layoutManager = layoutManager
         main_rv_list_data.adapter = adapter
 
+        /* Event listener untuk pindah ke activity AddStudentActivity
+        *
+        * */
         main_cv_add_data.onClick {
             startActivity(intentFor<AddStudentActivity>())
         }
     }
 
     private fun getListDataStudent(): List<StudentContract> {
-        var listData : List<StudentContract>? = null
+        var listData: List<StudentContract>? = null
         database.use {
             val result = select(StudentContract.TABLE_STUDENT)
             listData = result.parseList(classParser<StudentContract>())
         }
         return listData!!
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val listRefresh = getListDataStudent()
+        adapter = StudentAdapter(this, listRefresh)
+        adapter?.notifyDataSetChanged()
+        main_rv_list_data.adapter = adapter
     }
 }
